da320ce8498e17866150926262b066f209829063
nitishdrafts@gmail.com <nitishdrafts@gmail.com>
2024-03-15 13:00:27 +0000

    br3

--- models/example/staging/employe-detail/new1.sql
+++ models/example/staging/employe-detail/new1.sql
@@ -1,4 +1,4 @@
-{{ config(materialized='table', alias='emp12',tags=["new1"])}}
+{{ config(materialized='table', alias='emp_new',tags=["new1"])}}
 
 WITH source AS (
     SELECT emp_name, salary
@@ -8,5 +8,4 @@ WITH source AS (
 SELECT *
 FROM source
 ORDER BY salary DESC
-LIMIT 3
-
+LIMIT 1
