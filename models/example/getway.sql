779c944abd960d9d10db94710ee75b58328c0324
Nitish Pradhan <nitishdrafts@gmail.com>
2024-03-19 08:23:56 +0000

    yo

---
+++ models/example/getway.sql
@@ -0,0 +1,10 @@
+{{config(materialized='table', alias='Payment_Method_agg')}}
+
+SELECT
+    ORDER_ID,
+    {% for Payment_Method in ["credit_card/debit_card", "upi", "Net_Banking", "Mobile Wallet"] %}
+    SUM(CASE WHEN Payment_Method='{{Payment_Method}}' THEN amount ELSE 0 END) AS "{{Payment_Method}}_amount",
+    {% endfor %}
+    SUM(amount) AS total_amount
+FROM raw_data.payments
+GROUP BY 1;
