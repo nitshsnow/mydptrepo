883f22b873a5b8e59d3f25d216854e7197ce1557
Nitish Pradhan <nitishdrafts@gmail.com>
2024-03-19 11:06:50 +0000

    yt

--- models/example/getway.sql
+++ models/example/getway.sql
@@ -1,10 +1,10 @@
-{{config(materialized='table', alias='Method_agg')}}
+{{ config(materialized='table', alias='Payment_Method_agg') }}
 
 SELECT
     ORDER_ID,
-    {% for Payment_Method in ["credit_card/debit_card", "upi", "Net_Banking", "Mobile Wallet"] %}
-    SUM(CASE WHEN Payment_Method='{{Payment_Method}}' THEN amount ELSE 0 END) AS "{{Payment_Method}}_amount",
+    {% for Payment_Method in ["credit card/debit card", "UPI", "Net Banking", "Mobile Wallet"] %}
+    SUM(CASE WHEN payment_method = '{{Payment_Method}}' THEN amount END) AS "{{Payment_Method}}_amount",
     {% endfor %}
     SUM(amount) AS total_amount
-FROM raw_data.payments
-GROUP BY 1;
+FROM raw_data.PAYMENTS
+GROUP BY ORDER_ID;
