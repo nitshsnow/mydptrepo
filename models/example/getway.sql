3bf8be31267ee6da1f8e7fdebf2970b1f3376405
Nitish Pradhan <nitishdrafts@gmail.com>
2024-03-20 16:07:29 +0000

    hi

--- models/example/getway.sql
+++ models/example/getway.sql
@@ -1,8 +1,10 @@
 {{ config(materialized='table', alias='Payment_Method_agg') }}
 
+{% set pmlist=["credit card/debit card", "UPI", "Net Banking", "Mobile Wallet"]%}
+
 SELECT
     ORDER_ID,
-    {% for Payment_Method in ["credit card/debit card", "UPI", "Net Banking", "Mobile Wallet"] %}
+    {% for Payment_Method in pmlist %}
     SUM(CASE WHEN payment_method = '{{Payment_Method}}' THEN amount END) AS "{{Payment_Method}}_amount",
     {% endfor %}
     SUM(amount) AS total_amount
